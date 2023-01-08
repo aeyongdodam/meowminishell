50b34904e807159bc545ce204a83d74811c0f6b7
mkwon <mkwon@student.42seoul.kr>
2023-01-08 11:02:28 +0900

    fix exit error, 0, add his

--- main.c
+++ main.c
@@ -3,31 +3,30 @@
 
 int	g_exit_code;
 
-int main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
-	char	*line;
-	int		ret;
-	t_tree	*tree;
+	char		*line;
+	int			ret;
+	t_tree		*tree;
 	t_envnode	*envnode;
 
 	set_terminal();
 	prt_meows();
 	envnode = init_env(envp, tree);
-	while(1)
+	while (1)
 	{
 		set_signal_handler(0);
 		line = readline("\033[34mMinishell>\033[0m ");
-		if (line)
-		{
-			tree = lexer(line, envnode);
-			add_history(line);
-			if (find_error(tree))
-				continue;
-			main_pipe(tree, envnode, envp);
-			free(line);
-        }
-		else
+		if (!line)
 			prt_exit();
+		tree = lexer(line, envnode);
+		if (tree->root->left_child == NULL && tree->root->right_child == NULL)
+			continue ;
+		add_history(line);
+		if (find_error(tree))
+			continue ;
+		main_pipe(tree, envnode, envp);
+		free(line);
 	}
 	return (0);
 }
