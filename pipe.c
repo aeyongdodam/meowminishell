0753c8bbcdd076c88e2ec99ced6a3ff11bce932d
Sohyun Lee <sohlee@c4r2s7.42seoul.kr>
2023-01-14 04:08:31 +0900

    exit error

--- pipe.c
+++ pipe.c
@@ -493,7 +493,7 @@ void	main_pipe(t_tree *tree, t_envnode *envnode, char **envp)
 			exit (0);
 		}
 		else if (ft_strncmp(command[0], "exit", 5) == 0)
-			exit(builtin_exit(command));
+			exit(builtin_exit(command, 0));
 		else if (ft_strncmp(command[0], "$?", 3) == 0)
 		{
 			write(2, "meowshell: ", 12);
@@ -539,7 +539,7 @@ void	main_pipe(t_tree *tree, t_envnode *envnode, char **envp)
 		else if (ft_strncmp(tr->left_child->token->str, "export", 7) == 0)
 			g_exit_code = builtin_export(envnode, command, 1);
 		else if (ft_strncmp(tr->left_child->token->str, "exit", 5) == 0)
-			exit(builtin_exit(command));
+			exit(builtin_exit(command, 1));
 		else
 			g_exit_code = builtin_unset(envnode, command);
 	}
